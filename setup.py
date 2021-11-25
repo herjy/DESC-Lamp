b58b43582870da2f20cb7eb4f248b01007131fea
Remy Joseph <remyjoseph@ymail.com>
2021-11-25 04:56:41 -0800

    Packaging bug fix

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DESC-Lamp",
+    name="desclamp",
     version="0.0.1",
-    author="Rémy Joseph",
+    author="Remy Joseph",
     author_email="remy.joseph11@gmail.com",
-    description="DESC Strong Lensing postage stamp extraction and lensed source injection pipeline."
-    long_description="This section of DESC SL pipeline provides tools to  extract deep coadd images of strong gravitational lens candidates as well as light curves from DC2 and from the Rubin data. The preselection of lens candidates is done at the catalog levels. Postage stamps and light curves can then be used to conduct detailed strong gravitational lens searches, that usually rely on MAchine Learning techniques. This pipeline section also provides tools to insert synthetic lensed sources in Rubin (and DC2) images. Synthetic innjection will be used to train characterise lens finders.",
+    description="DESC Strong Lensing postage stamp extraction and lensed source injection pipeline.",
+    long_description="This section of DESC SL pipeline provides tools to  extract deep coadd images of strong gravitational lens candidates as well as light curves from DC2 and from the Rubin data. The preselection of lens candidates is done at the catalog levels. Postage stamps and light curves can then be used to conduct detailed strong gravitational lens searches, that usually rely on Machine Learning techniques. This pipeline section also provides tools to insert synthetic lensed sources in Rubin (and DC2) images. Synthetic injection will be used to train and characterise lens finders.",
     long_description_content_type="text/markdown",
     url="https://github.com/herjy/DESC-Lamp/",
     project_urls={
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: BSD 3-Clause License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "lamp"},
+    packages=setuptools.find_packages(where="lamp"),
     python_requires=">=3.6",
 )
