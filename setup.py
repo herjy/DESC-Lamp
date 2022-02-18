80570cbf7056cb05e84c54a3a545a1e8c1911ead
Rémy Joseph <remyjoseph@ymail.com>
2022-02-18 13:34:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="desclamp",
+    name="DESC-Lamp",
     version="0.0.1",
     author="Remy Joseph",
     author_email="remy.joseph11@gmail.com",
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: BSD 3-Clause License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "desclamp"},
-    packages=setuptools.find_packages(where="desclamp"),
+    package_dir={"": "DESC-Lamp"},
+    packages=setuptools.find_packages(where="DESC-Lamp"),
     python_requires=">=3.6",
 )
