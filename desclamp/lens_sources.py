dab015fa7e826ef44b852706de3d4be6188d7fa4
Remy Joseph <remyjoseph@ymail.com>
2022-02-21 10:23:07 -0800

    lensed source generation

--- desclamp/lens_sources.py
+++ desclamp/lens_sources.py
@@ -10,90 +10,78 @@ from lenstronomy.LensModel.lens_model import LensModel
 from lenstronomy.LightModel.light_model import LightModel
 from lenstronomy.Data.imaging_data import ImageData
 import lenstronomy.Util.simulation_util as sim_util
+from lenstronomy.Data.psf import PSF
 
 import galsim
 
 
-class LensedSource:
+class Lensing_frame:
     """ Object to described lensed sources"""
-    def __init__(self, spectrum, image=None, shape=(100, 100), shift=(0, 0), pix=0.2, wcs=None):
+    def __init__(self, shape=(100,100), pix = 0.2, wcs = None, hr_factor=1):
         """
         Source object that carries source and lens information and generates lensed source images for injection.
         Parameters
         ----------
-        gsobject: `galsim.GSObject`
-            a galsim representation for the source galaxy
-        spectrum: array
-            An array that contains the spectrum (colour) of the source.
         shape: `tuple`
-            Shape of the source galaxy patch in source plane
-        shift: tuple
-            shifts the source object in its patch. Default is (0,0): centered in the patch.
+            Shape of the image patch
         pix: `float`
-            pixel size in arcseconds. Default id Rubin's pixel.
+            pixel size in arcseconds. Default is Rubin's pixel.
         wcs: WCS
-            wcs information for the source patch.
-
+            wcs information for the source patch.  
+        hr_factor: `float`
+            the high resolution factor between source and lens plane
         """
-        if pix is None:
+        if pix is None: 
             assert wcs is not None
             try:
                 model_affine = wcs.wcs.pc
             except AttributeError:
                 model_affine = wcs.cd
-
             pix = np.sqrt(np.abs(model_affine[0, 0])* np.abs(model_affine[1, 1] - model_affine[0, 1] * model_affine[1, 0]))
-
+            
         self.pix = pix
-        self.shift = shift
-        self.image = image
+        self.hr_factor = hr_factor
         self.shape = shape
-        self.spectrum = spectrum
-        if self.image is not None:
-            self.source_image = self.draw_source()
+        self._source = None # An image of the source
+        self.source_args = None # source arguments dictionary
+    
+    def lens_source(self, lens_models, lens_args):
+        assert self.source is not None, "Please provide a source image."
 
-    def lens_source(self, lens_models, hr_factor, **kwargs):
-        assert self.image is not None, "Please provide a source image."
-
-        kwargs_data_high_res = sim_util.data_configure_simple(slef.shape*hr_factor, self.pix/hr_factor)
-        data_high_res = ImageData(**kwargs_data_high_res)
-
+        kwargs_image = sim_util.data_configure_simple(int(self.shape[0]),  # Issue with rectangle shapes
+                                                              self.pix)
+        image = ImageData(**kwargs_image)
+        
         lensModel = LensModel(lens_models)
-        source_light_model = LightModel(['INTERPOL'])
-
-        kwargs_lens
-        kwargs_source
-
-        image_model_high_res = ImageModel(
-            data_class=data_high_res,
-            lens_model_class=lensModel,
-            source_model_class=source_light_model)
-        lensed_image = image_model_high_res.image(
-            kwargs,
-            kwargs_source,
-            kwargs_lens_light=None,
-            kwargs_ps=None)
+        lightModel = LightModel(light_model_list=['INTERPOL'])
+        
+        imageModel = ImageModel(data_class=image, 
+                                         lens_model_class=lensModel, 
+                                         source_model_class=lightModel,
+                                         psf_class=PSF(psf_type='NONE'),
+                                         kwargs_numerics = {'supersampling_factor': self.hr_factor, 
+                                                            'supersampling_convolution': False})
+        lensed_image = imageModel.image(lens_args, self.source_args)
         return lensed_image
-
+    
     def draw_source(self):
         """ Draws a soource no a grid specified by the parameters of the __init__
         """
-        x, y = util.make_grid(numPix=self.shape, deltapix=1.)
         kwargs_list = [
-            {'image': self.image, 'scale': 1, 'phi_G': 0, 'center_x': self.shift[0], 'center_y': self.shift[1]}
+            {'image': self.source, 'scale': self.pix/self.hr_factor, 'phi_G': 0, 'center_x': 0, 'center_y': 0}
         ]
-        light_model = LightModel(light_model_list=['INTERPOL'])
-        return light_model.surface_brightness(x, y, kwargs_list)
-
+        
+        return kwargs_list
+    
     @property
-    def image(self):
-        return self.image
-
-    @image.setter
-    def image(self, image):
-        self.image = image
-        self.source_image = self.draw_source()
-
+    def source(self):
+        return self._source
+    
+    @source.setter
+    def source(self, source):
+        self._source = source
+        self.source_args = self.draw_source()
+    
     def from_gsobject(self, gsobject, smooth=0):
         """ Creates a Lensed source from a galsim object.
         Parameters
@@ -105,54 +93,61 @@ class LensedSource:
         """
         if smooth > 0:
             gso = galsim.Convolve(gsobject, galsim.Gaussian(sigma=smooth))
-        # Draws the galsim object on a grid
-        source = gso.drawImage(nx=shape[0],
-            ny=shape[1],
+        #Draws the galsim object on a grid    
+        source = gso.drawImage(nx=self.shape[0]*self.hr_factor,
+            ny=self.shape[1]*self.hr_factor,
             use_true_center = True,
             method='real_space',
-            scale=self.pix,
+            scale=self.pix/self.hr_factor,
             dtype=np.float64).array
-        self.image(source)
+        self.source = source
         return self
-
+        
     def from_lenstronomy(self, kwargs):
         """TO DO"""
         pass
-
-    @staticmethod
-    def from_galsim_parametric(profile='Sersic', **kwargs):
+    
+    def from_galsim_parametric(self, *args, profile='Spergel', shift=(0,0), shear=(0,0), **kwargs):
         """ Creates a Lensed source object from a source described as a parametric profile.
         Parameters
         ----------
-        spectrum: array
-            The spectrum of the the source
         profile: string
             The galsim profile to use to generate a source galaxy. Currently, Sersic, DeVeaucouleurs and Exponential are implemented
         kwargs:
             parameters of the profile
-
+            
         Returns
         -------
         A `Lensed_source` object.
         """
-
-        assert profile in ['Sersic', 'Exponential', 'DeVeaucouleurs'], \
-            "Not a valid profile. Please use 'Sersic', 'Exponential' or 'DeVeaucouleurs'."
-
+        
+        assert profile in ['Sersic', 'Exponential', 'DeVeaucouleurs', 'Spergel'], "Not a valid profile. Please use 'Sersic', 'Exponential' or 'DeVeaucouleurs'."
+        
         if profile == 'Sersic':
-            gso = galsim.Sersic(kwargs)
+            assert len(args)==1
+            gso = galsim.Sersic(n= args[0], **kwargs)
+        elif profile == 'Spergel':
+            assert len(args)==1
+            gso = galsim.Spergel(nu= args[0], **kwargs)
         elif profile == 'Exponential':
-            gso = galsim.Exponential(kwargs)
+            gso = galsim.Exponential(*args, *kwargs)
         else:
-            gso = galsim.DeVeaucouleurs(kwargs)
-
-        # Draws the galsim object on a grid
-        source = gso.drawImage(
-            nx=shape[0],
-            ny=shape[1],
-            use_true_center=True,
+            gso = galsim.DeVeaucouleurs(*args, *kwargs)
+            
+        gso = gso.shear(g1=shear[0],g2=shear[1])
+        gso = gso.shift(dx=shift[0],dy=shift[1])
+        
+        #Draws the galsim object on a grid    
+        source = gso.drawImage(nx=self.shape[0]*self.hr_factor,
+            ny=self.shape[1]*self.hr_factor,
+            use_true_center = True,
             method='real_space',
-            scale=self.pix,
+            scale=self.pix/self.hr_factor,
             dtype=np.float64).array
-        self.image(source)
+        self.source = source
         return self
+    
+    
+    
+    
+    
