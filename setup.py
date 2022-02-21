9be5186da4d65fb904469be05f2da17eafe339e4
Rémy Joseph <remyjoseph@ymail.com>
2022-02-21 19:06:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="descLamp",
+    name="desclamp",
     version="0.0.1",
     author="Remy Joseph",
     author_email="remy.joseph11@gmail.com",
