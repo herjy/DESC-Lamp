1b7acd133e57bdbd26259f8f1b676663834256aa
Rémy Joseph <remyjoseph@ymail.com>
2022-05-06 15:36:27 +0200

    Update test_td-env.sh

--- docker/test_td-env.sh
+++ docker/test_td-env.sh
@@ -3,6 +3,7 @@ set -eo pipefail
 source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 python -c "import lenstronomy"
+python -m pip install .
 pytest tests
 pytest --cov=desclamp
 exit 0
